457056fa9c8aff69d0c48a8e1e04d989899e5dbf
a2wd <github@a2wd.com>
2016-11-14 23:51:22 +0000

    Added second task in c to reverse strings

---
+++ 2.Problems/2.1.ArraysAndStrings/Reverse.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int length(char* str)
+{
+	if(str == NULL)
+	{
+		return 0;
+	}
+
+	int i = 0;
+	while(*(str + i) != '\0')
+	{
+		i++;
+	}
+
+	return i;
+}
+
+void reverse(char* str)
+{
+	int len = length(str);
+	if(len == 0 || len == 1) 
+	{
+		return;
+	}
+
+	for(int i = 0; i < (len/2); i++)
+	{
+		char temp = *(str + i);
+		*(str + i) = *(str + len - i - 1);
+		*(str + len - i - 1) = temp;
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	if(argc != 2)
+	{
+		printf("Usage: Reverse.exe input\nWhere input is a string to reverse");
+
+		return 1;
+	}
+
+	printf("%s reversed is: ", argv[1]);
+	reverse(argv[1]);
+	printf("%s\n", argv[1]);
+
+	return 0;
+}
