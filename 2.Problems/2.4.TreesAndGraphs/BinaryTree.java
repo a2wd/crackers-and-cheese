4a6a360a384d6a07a6144b5ed2744047eb8e5a3a
a2wd <github@a2wd.com>
2016-12-19 18:52:48 +0000

    Added initial implementations for binary tree & graph

---
+++ 2.Problems/2.4.TreesAndGraphs/BinaryTree.java
@@ -0,0 +1,13 @@
+// An implementation of a binary tree
+public class BinaryTree
+{
+	public BinaryTree left, right;
+	public int data;
+
+	BinaryTree(data)
+	{
+		this.data = data;
+		left = null;
+		right = null;
+	}
+}
---
+++ 2.Problems/2.4.TreesAndGraphs/Graph.java
@@ -0,0 +1,12 @@
+// An implementation of a graph
+public class Graph
+{
+	public ArrayList<Graph> children;
+	public int data;
+
+	Graph(data)
+	{
+		this.data = data;
+		children = new ArrayList<Graph>();
+	}
+}
