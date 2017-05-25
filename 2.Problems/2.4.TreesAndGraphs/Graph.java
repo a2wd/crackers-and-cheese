caf5fb77122ab379ec718c609a0ff5f318e5e594
a2wd <github@a2wd.com>
2017-05-25 08:35:22 +0100

    Updated readme, added import statement to Graph.java

--- 2.Problems/2.4.TreesAndGraphs/Graph.java
+++ 2.Problems/2.4.TreesAndGraphs/Graph.java
@@ -1,10 +1,12 @@
 // An implementation of a graph
+import java.util.ArrayList;
+
 public class Graph
 {
 	public ArrayList<Graph> children;
 	public int data;
 
-	Graph(data)
+	Graph(int data)
 	{
 		this.data = data;
 		children = new ArrayList<Graph>();
